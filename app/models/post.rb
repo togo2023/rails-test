254a5c69e3de81a4c4173b5f12c2558887c2f062
togo <kuraisut@gmail.com>
2023-11-05 13:30:44 +0900

    終了日が開始日より前にならないようにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ApplicationRecord
   validates :start_day, presence: true
   validates :end_day, presence: true
   validates :content, length: {maximum: 500}
+  validate :end_day_time
+
+  private
+
+  def end_day_time
+    return false if end_day.blank? || start_day.blank?
+    if start_day > end_day
+      errors.add(:end_day, "は開始日より後の日付を選択してください")
+    end
+  end
 end
